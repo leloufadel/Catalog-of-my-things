18b9118aaf61b2ccb5910200b076f4eb07b73b9b
Tchasinga jacques <tchasingajacques@gmail.com>
2023-09-20 12:06:08 +0200

    new commit checked

--- Game and author/author.rb
+++ Game and author/author.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Author
   attr_accessor :name, :items
 
@@ -7,9 +9,21 @@ class Author
   end
 
   def add_item(item)
-    # Add the input item to the collection of items
     @items << item
-    # Set self as a property of the item object
     item.author = self
+  end
+
+  def to_json(*_args)
+    {
+      name: @name,
+      items: @items.map(&:to_json)
+    }.to_json
+  end
+
+  def self.from_json(json)
+    data = JSON.parse(json)
+    new(data['name']).tap do |author|
+      author.items = data['items'].map { |item_data| Game.from_json(item_data) }
+    end
   end
 end
