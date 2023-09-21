051ddafc93552d9bdb6f8f2de388e5e300b8ee2d
binyamT <binyampowerhc@gmail.com>
2023-09-21 19:00:15 +0300

    update the Item class in separate file

--- item.rb
+++ item.rb
@@ -1,11 +1,12 @@
 require 'date'
 
 class Item
-  attr_accessor :publish_date, :id, :can_be_archived, :label
+  attr_accessor :publish_date, :id, :can_be_archived, :label, :genre, :author
 
   def initialize(publish_date, id = nil)
     @id = id || Random.rand(1..1000)
     @publish_date = publish_date
+    @archived = archived
   end
 
   def add_label(label)
@@ -13,9 +14,18 @@ class Item
     @label.add_item(self)
   end
 
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
   def can_be_archived?
     current_year = Date.today.year
     publish_year = Date.parse(@publish_date).year
     current_year - publish_year > 10
+  end
+  
+  def move_to_archive
+    @archived = true if can_be_archived?
   end
 end
