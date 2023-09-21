392bc2966c5852506a59ccb75e674e1b3667de91
leloufadel <leloufadel@gmail.com>
2023-09-21 10:50:24 +0300

    add function for display book and label, load book-label and preserve the data

---
+++ Books/modules/modules.rb
@@ -0,0 +1,40 @@
+require_relative 'book/addbook'
+require_relative 'book/displaybook'
+require_relative 'book/displaylabel'
+require_relative 'book/load_book_label'
+require_relative 'book/preservedata'
+
+
+module Modules
+  include AddBook
+  include DisplayBook
+  include DisplayLabel
+  include PreserveData
+  include LoadData
+
+
+  def save_collections
+    save_book
+    save_label
+   
+  end
+
+  def load_collections
+    load_labels
+    load_books
+   
+  end
+
+# def load_collections
+#     puts 'Loading data from JSON files...'
+    
+#     load_labels
+#     puts "Loaded #{@label.size} labels."
+  
+#     load_books
+#     puts "Loaded #{@books.size} books."
+  
+#     puts 'Data loading complete.'
+#   end
+  
+end
